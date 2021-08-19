2f5ad4913812523641e6aa28490348e9061fdd3b
abhi-amr <abhi-amr@users.noreply.github.com>
2021-08-19 16:15:35 +0530

    updated mddleware

---
+++ RequestResponseModification/Middleware/CryptographyService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Azure.Cosmos;
+using RequestResponseModification.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestResponseModification.Middleware
+{
+    public class CryptographyService
+    {
+        private RSAParameters _publicKeyClient;
+        private RSAParameters _privateKeyServer;
+        private readonly RSACryptoServiceProvider _rsa = null;
+        private readonly Database _dbClient = null;
+
+
+        public CryptographyService(Database dbclient)
+        {
+            _dbClient = dbclient;
+            _rsa = new RSACryptoServiceProvider(2048);
+            _rsa.PersistKeyInCsp = false; // for not storing the keys in container
+
+            //temporary sol for storing keys
+            _publicKeyClient = GetClientPublicKeyFromCosmos().GetAwaiter().GetResult();
+            _privateKeyServer = GetServerPrivateKeyFromCosmos().GetAwaiter().GetResult();
+        }
+
+        public string Encrypt(string data)
+        {
+            //string[] splitted = data.Split('"');   //because "\"HFJHFE"
+            _rsa.ImportParameters(_publicKeyClient);
+            var byteData = Encoding.UTF8.GetBytes(data);
+            var encryptedMessage = _rsa.Encrypt(byteData, false);
+            return Convert.ToBase64String(encryptedMessage);
+        }
+
+        public string Decrypt(string input)
+        {
+            string[] splitted = input.Split('"');   //because "\"HFJHFE"
+            _rsa.ImportParameters(_privateKeyServer);
+            var byteData = Convert.FromBase64String(splitted[1]);
+            var plainMessage = _rsa.Decrypt(byteData, false);
+            return Encoding.UTF8.GetString(plainMessage);
+        }
+
+        private async Task<RSAParameters> GetClientPublicKeyFromCosmos()
+        {
+            var cryptoKeyContainerClient = _dbClient.GetContainer("cryptoKey");
+            var resp = await cryptoKeyContainerClient.ReadItemAsync<CryptoKey>("ClientPublicKey", new PartitionKey("Client"));
+
+            return resp.Resource.Key;
+        }
+
+        private async Task<RSAParameters> GetServerPrivateKeyFromCosmos()
+        {
+            var cryptoKeyContainerClient = _dbClient.GetContainer("cryptoKey");
+            var resp = await cryptoKeyContainerClient.ReadItemAsync<CryptoKey>("ServerPrivateKey", new PartitionKey("Server"));
+
+            return resp.Resource.Key;
+        }
+    }
+}
--- RequestResponseModification/Middleware/MyMiddleware.cs
+++ RequestResponseModification/Middleware/MyMiddleware.cs
@@ -15,151 +15,97 @@ namespace RequestResponseModification.Middleware
 {
     public class MyMiddleware : IMiddleware
     {
-        private RSAParameters _publicKeyClient;
-        private RSAParameters _privateKeyServer;
-        private readonly RSACryptoServiceProvider _rsa = null;
-        private readonly Database _dbClient = null;
-        private readonly ILogger<MyMiddleware> _logger;
+        private readonly CryptographyService _cryptoService;
 
-        public MyMiddleware(Database dbclient,
-            ILogger<MyMiddleware> logger)
+        public MyMiddleware(CryptographyService cryptoService)
         {
-            _dbClient = dbclient;
-            _logger = logger;
-            _rsa = new RSACryptoServiceProvider(2048);
-            _rsa.PersistKeyInCsp = false; // for not storing the keys in container
-
-            //temporary solution for storing public and private key...ignore
-            _publicKeyClient = GetClientPublicKeyFromCosmos().GetAwaiter().GetResult();
-            _privateKeyServer = GetServerPrivateKeyFromCosmos().GetAwaiter().GetResult();
+            _cryptoService = cryptoService;
         }
+
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-            await ModifyRequest(context);
-            await next(context);
-            await ModifyResponse(context);
+            await ModifyStream(context, next);
         }
 
-        private async Task ModifyResponse(HttpContext context)
+        private async Task ModifyStream(HttpContext context, RequestDelegate next)
         {
-            var originalResponseStream = context.Response.Body;
-
-            using (var ms = new MemoryStream())
-            {
-                context.Response.Body = ms;
-
-
-                ms.Position = 0;
-                var responseReader = new StreamReader(ms);
-
-                var responseContent = responseReader.ReadToEnd();
-                Console.WriteLine($"PlainResponse Body: {responseContent}");
-
-                var encryptedResponse = Encrypt(responseContent);
-                var byteData = Encoding.UTF8.GetBytes(encryptedResponse);
-                await ms.WriteAsync(byteData, 0, byteData.Length);
-
-                ms.Position = 0;
-
-                await ms.CopyToAsync(originalResponseStream);
-                context.Response.Body = originalResponseStream;
-            }
-        }
-
-        private async Task ModifyRequest(HttpContext context)
-        {
-            //context.Request.EnableBuffering();
             var request = context.Request;
-
-            //get the request body and put it back for the downstream items to read
-            var stream = request.Body; //currently holds the original stream                    
-            var reader = new StreamReader(stream);
-            var originalContent = await reader.ReadToEndAsync();
+            var stream = request.Body;// currently holds the original stream                    
+            var originalReader = new StreamReader(stream);
+            var originalContent = await originalReader.ReadToEndAsync();
             var notModified = true;
-            Console.WriteLine("\nmiddleware request : " + originalContent);
+            Console.WriteLine($"incoming request in middleware : {originalContent}");
             try
             {
-                /*------------------this part modify the request--------------------------*/
-                //var dataSource = JsonConvert.DeserializeObject<Master>(originalContent);
+
                 if (originalContent != null)
                 {
-                    var modifydata = new Master() { Id = "Changed in middleware", Email = "changed" };
-                    var json = JsonConvert.SerializeObject(modifydata);
-                    var requestData = Encoding.UTF8.GetBytes(json);
+                    var decryptedData = _cryptoService.Decrypt(originalContent);
+                    Console.WriteLine($"outgoing request from middleware : {decryptedData}");
+                    //convert string to jsontype
+                    //var json = JsonConvert.SerializeObject(decryptedData); //uncomment for string
+                    //modified stream
+                    var requestData = Encoding.UTF8.GetBytes(decryptedData);
+                    //var requestData = Encoding.UTF8.GetBytes(json);
                     stream = new MemoryStream(requestData);
                     notModified = false;
                 }
-                /*--------------------------------------------*/
-
-
-
-                /*------------------this part sends null to the controller--------------------------*/
-                /*---------and this is required for encryption decryption to change the whole string content-------------------*/
-                //var dataSource = originalContent;
-                //if (originalContent != null)
-                //{
-                //    //replace request stream to downstream handlers
-                //    var requestData = Encoding.UTF8.GetBytes("Hola Server. Modified Request");
-                //    stream = new MemoryStream(requestData);
-                //    notModified = false;
-                //}
-
-                /*------------------------------------------------*/
-
-
             }
             catch (Exception e)
             {
-                _logger.LogError(e.Message);
+                Console.WriteLine(e.Message);
             }
             if (notModified)
             {
-                //put original data back for the downstream to read
+                //putting original data
                 var requestData = Encoding.UTF8.GetBytes(originalContent);
                 stream = new MemoryStream(requestData);
             }
-            //stream.Position = 0;
+
             request.Body = stream;
+
+            //------------response---------------
+
+            var reponseContent = string.Empty;
+
+            // Store the "pre-modified" response stream.
+            var existingBody = context.Response.Body;
+
+            using (var newBody = new MemoryStream())
+            {
+                // We set the response body to our stream so we can read after the chain of middlewares have been called.
+                context.Response.Body = newBody;
+
+                await next(context);
+
+                // Set the stream back to the original.
+                context.Response.Body = existingBody;
+
+                newBody.Seek(0, SeekOrigin.Begin);
+
+                //reponseContent will be `Hello Server!...`
+                //reponseContent = new StreamReader(newBody).ReadToEnd();
+
+                //check this---------
+                var contentReader = new StreamReader(newBody);
+                reponseContent = await contentReader.ReadToEndAsync();
+
+                //----------
+
+                //Console.WriteLine($"incoming response in middleware : {newContent}");
+                //string[] splitted = newContent.Split('"');
+                //var encryptedData = _cryptoService.Encrypt(splitted[1]);
+
+                var encryptedData = _cryptoService.Encrypt(reponseContent);
+
+                Console.WriteLine($"outgoing response from in middleware : {encryptedData}");
+
+                // Send our modified content to the response body.
+                await context.Response.WriteAsync(encryptedData);
+
+            }
         }
-
-        #region Encryption/Decryption
-
-        private string Encrypt(string data)
-        {
-            _rsa.ImportParameters(_publicKeyClient);
-            var byteData = Encoding.UTF8.GetBytes(data);
-            var encryptedMessage = _rsa.Encrypt(byteData, false);
-            return Convert.ToBase64String(encryptedMessage);
-        }
-
-        private string Decrypt(string input)
-        {
-            _rsa.ImportParameters(_privateKeyServer);
-            var byteData = Convert.FromBase64String(input);
-            var plainMessage = _rsa.Decrypt(byteData, false);
-            return Encoding.UTF8.GetString(plainMessage);
-        }
-
-        #endregion
-
-        #region Cosmos
-
-        private async Task<RSAParameters> GetClientPublicKeyFromCosmos()
-        {
-            var cryptoKeyContainerClient = _dbClient.GetContainer("cryptoKey");
-            var resp = await cryptoKeyContainerClient.ReadItemAsync<CryptoKey>("ClientPublicKey", new PartitionKey("Client"));
-
-            return resp.Resource.Key;
-        }
-
-        private async Task<RSAParameters> GetServerPrivateKeyFromCosmos()
-        {
-            var cryptoKeyContainerClient = _dbClient.GetContainer("cryptoKey");
-            var resp = await cryptoKeyContainerClient.ReadItemAsync<CryptoKey>("ServerPrivateKey", new PartitionKey("Server"));
-
-            return resp.Resource.Key;
-        }
-
-        #endregion
+    
+    
     }
 }
